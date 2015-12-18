723e214e0a00ac192ec57411888f688c9e731106
sergiub <buciuc_sergiu@yahoo.com>
2015-12-18 20:32:51 +0200

    server code refactoring

--- oauth.js
+++
@@ -1,9 +0,0 @@
-const oauthIds = {
-  strava: {
-    clientID: '9244',
-    clientSecret: '94b8617916ead111be4ebf700ca1d610e2fb2ff0',
-    callbackURL: 'http://127.0.0.1:3000/auth/strava/callback'
-  }
-};
-
-export default oauthIds;
--- server.js
+++ server.js
@@ -2,50 +2,37 @@ import express from 'express';
 import swig from 'swig';
 import serveStatic from 'serve-static';
 import serveIndex from 'serve-index';
+import session from 'express-session';
+import fs from 'fs';
 
-import * as StravaAuth from './auth';
-import * as Strava from './strava';
+import * as StravaPassport from './app/services/strava-passport';
 
+const app = express();
 
-export default function startServer() {
-  const app = express();
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/public/views');
+app.use(serveStatic(__dirname + '/public'));
+app.use('/public', serveIndex(__dirname + '/public'));
+app.use(session({
+  secret: 'myapp',
+  resave: false,
+  saveUninitialized: false
+}));
 
-  app.engine('html', swig.renderFile);
-  app.set('view engine', 'html');
-  app.set('views', __dirname + '/public/views');
-  app.use(serveStatic(__dirname + '/public'));
-  app.use('/public', serveIndex(__dirname + '/public'));
+StravaPassport.init(app);
 
-  StravaAuth.init(app);
-  app.get('/', (req, res) => {
-    if (StravaAuth.isAuthenticated()) {
-      Strava.getActivities().then(
-        activities => res.render('index', {
-          'title': 'Activities',
-          'activities': JSON.stringify(activities.map(activity => ({
-            id: activity.id,
-            name: activity.name,
-            type: activity.type,
-            startDate: activity.start_date,
-            movingTime: activity.moving_time
-          })))
-        })
-      ).catch(err => res.send({
-        'access token': StravaAuth.getToken(),
-        err
-      }));
-    } else {
-      res.redirect('/login');
-    }
-  });
+// dynamically include routes (Controller)
+fs.readdirSync('./app/controllers').forEach(function (file) {
+  if(file.substr(-3) == '.js') {
+      var route = require('./app/controllers/' + file);
+      route.controller(app);
+  }
+});
 
-  app.get('/login', (req, res) => res.render('login', {title: 'Login'}));
-  app.get('/auth/strava', StravaAuth.authenticate);
+const server = app.listen(3000, () => {
+  let host = server.address().address;
+  let port = server.address().port;
 
-  const server = app.listen(3000, () => {
-    let host = server.address().address;
-    let port = server.address().port;
-
-    console.log(`Listening on ${host}:${port}`);
-  });
-}
+  console.log(`Listening on ${host}:${port}`);
+});
